6f8c0169ba79d16cc2a25f92a5c24560691aaf50
krshna-dtdl <129184791+krshna-dtdl@users.noreply.github.com>
2025-01-12 01:32:42 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,1 @@
-import math; a, b, c = 1, -3, 2; print(f"The roots of the quadratic equation are: {(((-b + math.sqrt(b**2 - 4*a*c)) / (2*a), (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)) if b**2 - 4*a*c > 0 else (-b / (2*a),) if b**2 - 4*a*c == 0 else 'No real roots')}")
+import math; a, b, c = 1, -3, 2; print(f"The roots of the quadratic equation are: {(((-b - math.sqrt(b**2 - 4*a*c)) / (2*a), (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)) if b**2 - 4*a*c > 0 else (-b / (2*a),) if b**2 - 4*a*c == 0 else 'No real roots')}")
